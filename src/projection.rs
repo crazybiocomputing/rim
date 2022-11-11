0321ee759bb6507b560d8c2c51864b3961afbac9
Leonard Bridel <leonardbrindel@yahoo.fr>
2022-11-11 18:49:10 +0100

    premier jet pas terrible

---
+++ src/projection.rs
@@ -0,0 +1,34 @@
+//
+//  RIM - Rust Image
+//  Copyright (C) 2022  Jean-Christophe Taveau, Léonard Brindel, Océane Dorémus, Léo Gillet.
+//
+//  This file is part of RIM
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with RIM.  If not, see <http://www.gnu.org/licenses/>.
+
+fn pub image_projection(ImageStack ist) -> Vec{
+   let img_projection_stack = Vec::new();
+   for img in 10 {
+        img_projection_stack.push(ist);
+   }
+   return img_projection_stack;
+}
+
+fn pub image_stack_projection(ImageStacks ist_stack) -> Vec{
+    let all_img_projection = Vec::new();
+    for img_stack in ist_stack{
+        all_img_projection.push(image_projection(img_stack);
+    }
+    return all_img_projection;
+}
