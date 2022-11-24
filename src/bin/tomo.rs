f3ab1ce1b6d5d5d21eb0155a2b25471cf04c504a
Mathieu Arru <mathieu.arru.n@gmail.com>
2022-11-24 16:52:06 +0100

    decomposition, nearest, struct, debug

--- src/bin/tomo.rs
+++ src/bin/tomo.rs
@@ -1,5 +1,60 @@
 use rim::cryoem::tomo_re::*;
+use rim::float_processor::FloatProcessor;
+use rim::color_space::ColorSpace;
+use rim::grayscale::Gray;
+use std::env;
+use rim::io::image_writer::*;
+use rim::io::file_info::*;
+use rim::io::image_reader::*;
+use rim::io::text_reader::*;
+
+// Commande : cargo run --bin sinogram -- -i "path_image" -d 256x256 -b 32 -p "path_ficher_angle" -o "path_sortie_image"
+
+// Exemple:  cargo run --bin sinogram -- -i ./src/sino.bin -d 256x256 -b 32 -p ./src/angles.csv -o ./test
 
 fn main() {
     tomography();
+    /* 
+    let args: Vec<String> = env::args().collect();
+    let mut input = String::new();
+    let mut output = String::new();
+    let mut params = String::new();
+    let mut meta : (u32,u32,usize) = (0,0,0);
+    
+    // Loop over the arguments
+    match args.len() {
+        1 => {
+            println!("Arguments required.");
+           
+        },
+        11 => {
+            for i in (1..args.len()).step_by(2) {
+                let arg = &args[i];
+                let val = &args[i+1];
+                // Parse 
+                match &arg[..] {
+                "-i" => input = val.to_string(),
+                "-d" | "--dim" => {
+                    let words : Vec<&str> = val.split('x').collect();
+                    meta.0 = words[0].parse::<u32>().unwrap() as u32;
+                    meta.1 = words[1].parse::<u32>().unwrap() as u32;
+                },
+                "-b" | "--bpp" => meta.2 = val.parse::<u32>().unwrap() as usize,
+                "-p" => params = val.to_string(),
+                "-o" => output = val.to_string(),
+                _ => panic!("Unknown argument")
+                }
+            }
+            let ip =import_image(&input,meta.0,meta.1,meta.2,params,&output);
+            let ip2 = Sinogram::new_in_range(&ip,0.0,180.0,45.0);
+            let op =OutputProcessor::FloatProcessor(ip2);
+            FileSaver::save_processor(&output, FileInfo::GRAY32_FLOAT, op);
+
+        }
+        _ => {
+            println!("Missing arguments. {}",args.len());
+          
+        }
+    }
+    */
 }
--- src/cryoem/tomo_re.rs
+++ src/cryoem/tomo_re.rs
@@ -1,139 +1,185 @@
-use crate::{image_processor::ImageProcessor, grayscale::Gray32, color_space::{ColorSpace, Space}, float_processor::FloatProcessor, image_stack::ImageStack, io::{image_reader::OutputProcessor, image_writer::FileSaver}};
-use std::{f64::consts::PI, vec, ops::Index};
+//
+//  RIM - Rust Image
+//  Copyright (C) 2022  Jean-Christophe Taveau.
+//
+//  This file is part of RIM
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with RIM.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Authors: Mathieu Arru, Léa Chabot
+
+use crate::{grayscale::Gray32, color_space::ColorSpace, float_processor::FloatProcessor};
+use std::{f64::consts::PI, vec};
 use crate::image_traits::Access;
-use crate::io::file_info::*;
 
-pub fn tomography() {
+pub struct Section {}
 
-    let section = vec![
-        0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000,
-        0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000,
-        0.000,0.000,1.000,1.000,0.000,0.000,0.000,0.000,
-        0.000,0.000,0.000,1.000,1.000,0.000,0.000,0.000,
-        0.000,0.000,0.000,1.000,0.000,0.000,0.000,0.000,
-        0.000,0.000,0.000,1.000,1.000,1.000,0.000,0.000,
-        0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000,
-        0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000];
+impl Section {
 
-    let chessboard = vec![
-        255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32,
-        0f32, 255f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 0f32, 255f32, 0f32, 255f32,
-        0f32, 255f32, 0f32, 255f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 0f32, 255f32,
-        0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32,
-        0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32,
-    ];
+    pub fn new(ip: &FloatProcessor) -> FloatProcessor {
+        let mut result : Vec<f32> = vec![0.0 ; (ip.get_width()*ip.get_width()) as usize];
+        let angles = Self::compute_angles(ip);
 
-    let sinogram = vec![
-        0.000,0.000,0.245,1.000,0.498,0.245,0.000,0.000,
-        0.087,0.000,0.253,0.956,0.640,0.174,0.024,0.024,
-        0.119,0.024,0.245,0.925,0.814,0.079,0.024,0.087,
-        0.087,0.047,0.435,0.672,0.727,0.119,0.024,0.119,
-        0.024,0.047,0.601,0.451,0.617,0.348,0.000,0.087,
-        0.000,0.000,0.751,0.245,0.498,0.498,0.000,0.000,
-        0.000,0.111,0.577,0.308,0.474,0.435,0.142,0.119,
-        0.055,0.166,0.451,0.395,0.451,0.427,0.229,0.150,
-        0.055,0.142,0.292,0.553,0.490,0.340,0.206,0.150,
-        0.000,0.095,0.261,0.514,0.743,0.324,0.126,0.119,
+        for row in 0..angles.len(){ 
+            let bp = Self::back_projection_nearest(&ip, angles[row], row as u32);
+            for y in 0..ip.get_width(){
+                for x in 0..ip.get_width(){
+                    let index = x + ip.get_width()*y;
+                    result[index as usize] += bp[x as usize];
+                }
+            }
+            println!("{:?}", result);
+        }
+        FloatProcessor::new(ip.get_width(),ip.get_width(), result as Vec<f32>, Gray32::new())
+    }
+
+    // Back projection for an angle (linear)
+    pub fn back_projection_linear(ip : &FloatProcessor, angle : f32, y : u32) -> Vec<f32> {
+        let index = Self::rotate(ip, angle, y);
+        Self::interploate_linear(ip, index, y)
+    }
+
+    // Back projection for an angle (nearest)
+    pub fn back_projection_nearest(ip : &FloatProcessor, angle : f32, y : u32) -> Vec<f32> {
+        let index = Self::rotate(ip, angle, y);
+        Self::nearest(ip, index, y)
+    }
+
+
+    pub fn compute_angles(ip : &FloatProcessor) -> Vec<f32> {
+        let mut angles: Vec<f32> = Vec::new();
+        let step = (180/ip.get_height()) as f32;
+            for i in 0..ip.get_height() {
+                angles.push(-1.0 * i as f32 * step);
+            }
+        angles
+    }
+
+    pub fn rotate(ip: &FloatProcessor, angle: f32, y: u32) -> Vec<(f32,f32)> {
+        let mut index = Vec::new();
+
+        //Compute center
+        let center: f32 = (ip.get_width()as f32/2.0).round();
+
+        // Convert the angle to radians
+        let angle_rad: f32 = (angle as f32) * PI as f32 / 180.0;
+
+        // Compute x' and y'
+        for x in 0..ip.get_width(){
+            let x_prime: f32 = (((x as f32) - center )*angle_rad.cos() - ((y as f32) - center)*angle_rad.sin()) as f32;
+            let y_prime: f32 = (((x as f32) - center )*angle_rad.sin() + ((y as f32) - center)*angle_rad.cos()) as f32;
+            index.push((x_prime, y_prime));
+            println!("XPRIME AVANT CENTER {x_prime}, YPRIME {y_prime}, ANGLE {angle}");
+        }
+        index
+    }
+
+    pub fn interploate_linear(ip: &FloatProcessor, index: Vec<(f32,f32)>, y: u32) -> Vec<f32>{
+        let mut result : Vec<f32> = vec![0.0 ; ip.get_width() as usize];
+
+        //Compute center
+        let center: f32 = (ip.get_width()as f32/2.0).round();
+
+        for x in 0..ip.get_width(){
+            let x_prime = index[x as usize].0 + center;
+            if x_prime < 8.0 && x_prime > -1.0{
+                let x_floor = x_prime.floor();
+                let x_ceil = x_prime.ceil();
+                let x_floor_prop = x_ceil - x_prime;
+                let x_ceil_prop = 1.0 - x_floor_prop;
+                
+                if x_floor >= 0.0 {
+                    result[x_floor as usize] += ip.get_pixel_at(x as u32, y as u32).unwrap() as f32 * x_floor_prop; 
+                }
+                if x_ceil <= 7.0 {
+                    result[x_ceil as usize] += ip.get_pixel_at(x as u32, y as u32).unwrap() as f32 * x_ceil_prop; 
+                }
+            }
+        }
+        result
+    }
+
+    pub fn nearest(ip: &FloatProcessor, index: Vec<(f32,f32)>, y: u32) -> Vec<f32>{
+        let mut result : Vec<f32> = vec![0.0 ; ip.get_width() as usize];
+
+        //Compute center
+        let center: f32 = (ip.get_width()as f32/2.0).round();
+
+        for x in 0..ip.get_width(){
+            let x_prime = index[x as usize].0.round() + center;
+            if x_prime >= 0.0 && x_prime < ip.get_width() as f32{
+                result[x_prime as usize] += ip.get_pixel_at(x as u32, y as u32).unwrap() as f32;
+                println!("XPRIME {x_prime} | VALUE ADD{}", ip.get_pixel_at(x as u32, y as u32).unwrap());
+            }
+        }
+    println!("ROW ADD{:?}", result);
+    result
+    }
+        
+
+}
+    
+
+//#[cfg(test)]
+//mod tests {
+
+    use super::*;
+    use crate::io::{image_reader::OutputProcessor, image_writer::FileSaver};
+    use crate::image_processor::ImageProcessor;
+    use crate::io::file_info::FileInfo;
+
+    //#[test]
+    pub fn tomography() {
+
+        let section = vec![
+            0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000,
+            0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000,
+            0.000,0.000,1.000,1.000,0.000,0.000,0.000,0.000,
+            0.000,0.000,0.000,1.000,1.000,0.000,0.000,0.000,
+            0.000,0.000,0.000,1.000,0.000,0.000,0.000,0.000,
+            0.000,0.000,0.000,1.000,1.000,1.000,0.000,0.000,
+            0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000,
+            0.000,0.000,0.000,0.000,0.000,0.000,0.000,0.000];
+
+        let chessboard = vec![
+            255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32,
+            0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32,
+            255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32,
+            0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32,
+            255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32,
+            0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32,
+            255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32,
+            0f32, 255f32, 0f32, 255f32, 0f32, 255f32, 0f32, 255f32,
         ];
 
+        let sinogram = vec![
+            0.000,0.000,0.245,1.000,0.498,0.245,0.000,0.000,
+            0.087,0.000,0.253,0.956,0.640,0.174,0.024,0.024,
+            0.119,0.024,0.245,0.925,0.814,0.079,0.024,0.087,
+            0.087,0.047,0.435,0.672,0.727,0.119,0.024,0.119,
+            0.024,0.047,0.601,0.451,0.617,0.348,0.000,0.087,
+            0.000,0.000,0.751,0.245,0.498,0.498,0.000,0.000,
+            0.000,0.111,0.577,0.308,0.474,0.435,0.142,0.119,
+            0.055,0.166,0.451,0.395,0.451,0.427,0.229,0.150,
+            0.055,0.142,0.292,0.553,0.490,0.340,0.206,0.150,
+            0.000,0.095,0.261,0.514,0.743,0.324,0.126,0.119,
+            ];
 
-    let sino_ip = ImageProcessor::new(8,8, chessboard, Gray32::new());
 
-    let op = OutputProcessor::FloatProcessor(test);
-    FileSaver::save_processor("./src/cryoem/test_backprojection", FileInfo::GRAY32_FLOAT, op)
-}
-
-
-
-
-// Use back_projection with all angles compute
-pub fn new(ip: &FloatProcessor, angle_list : &Vec<f32>) -> FloatProcessor {
-    let num = angle_list.len();
-    let mut r : Vec<f32> = vec![0.0 ; (ip.get_width()*ip.get_width()) as usize];
-    for i in 0..num {
-        let angle : f32 =  angle_list[i];
-        println!("{angle}");
-        let bp = back_projection(&ip, angle, i as u32);
-        for y in 0..ip.get_width(){
-            for x in 0..ip.get_width(){
-                let index = x + ip.get_width()*y;
-                r[index as usize] += bp[x as usize];
-                
-            }
-        }
+        let sino_ip = ImageProcessor::new(8,10, sinogram, Gray32::new());
+        let test = Section::new(&sino_ip);
+        let op = OutputProcessor::FloatProcessor(test);
+        FileSaver::save_processor("./src/cryoem/test_backprojection", FileInfo::GRAY32_FLOAT, op)
     }
-    //return the section
-    FloatProcessor::new(ip.get_width(),ip.get_width(),r as Vec<f32>, Gray32::new())
-}
-
-pub fn back_projection_all(ip: &FloatProcessor, angle : f32) -> FloatProcessor {
-    let mut result : Vec<f32> = vec![0.0 ; (ip.get_width()*ip.get_width()) as usize];
-
-    // boucle y 
-    // compute angles
-    // appeller les fonctions 
-    let bp = back_projection(&ip, angle, y);
-    for y in 0..ip.get_width(){
-        for x in 0..ip.get_width(){
-            let index = x + ip.get_width()*y;
-            result[index as usize] += bp[x as usize];
-        }
-    }
-    FloatProcessor::new(ip.get_width(),ip.get_width(), result as Vec<f32>, Gray32::new())
-    }
-
-// Back projection for an angle
-pub fn back_projection(ip : &FloatProcessor, angle : f32, y : u32) -> Vec<f32> {
-    let index = rotate(ip, angle, y);
-    interploate_linear(ip, index, y)
-}
-
-
-pub fn compute_angles(ip : &FloatProcessor) -> Vec<f32> {
-    let mut angles: Vec<f32> = Vec::new();
-    let step = (180/ip.get_height()) as f32;
-        for i in 0..ip.get_height() {
-            angles.push(-1.0 * i as f32 * step);
-        }
-    angles
-}
-
-pub fn rotate(ip: &FloatProcessor, angle: f32, y: u32) -> Vec<(f32,f32)> {
-    let mut index: Vec<(f32,f32)>;
-
-    //Compute center
-    let center: f32 = (ip.get_width()as f32/2.0).round();
-
-    // Convert the angle to radians
-    let angle_rad: f32 = (angle as f32) * PI as f32 / 180.0;
-
-    // Compute x' and y'
-    for x in 0..ip.get_width(){
-        let x_prime: f32 = ((((x as f32) - center )*angle_rad.cos() - ((y as f32) - center)*angle_rad.sin()) as f32) + center;
-        let y_prime: f32 = ((((x as f32) - center )*angle_rad.sin() + ((y as f32) - center)*angle_rad.cos()) as f32) + center;
-        index.push((x_prime, y_prime));
-    }
-    index
-}
-
-pub fn interploate_linear(ip: &FloatProcessor, index: Vec<(f32,f32)>, y: u32) -> Vec<f32>{
-    let mut result : Vec<f32> = vec![0.0 ; ip.get_width() as usize];
-
-    for x in 0..ip.get_width(){
-        let x_prime = index[x as usize].0;
-        if x_prime <= 8.0 && x_prime >= -1.0{
-            let x_floor = x_prime.floor();
-            let x_ceil = x_prime.ceil();
-            let x_floor_prop = x_ceil - x_prime;
-            let x_ceil_prop = 1.0 - x_floor_prop;
-            
-            if x_floor >= 0.0 {
-                result[x_floor as usize] += ip.get_pixel_at(x as u32, y as u32).unwrap() as f32 * x_floor_prop; 
-            }
-            if x_ceil <= 7.0 {
-                result[x_ceil as usize] += ip.get_pixel_at(x as u32, y as u32).unwrap() as f32 * x_ceil_prop; 
-            }
-        }
-    }
-    result
-}
+//}
