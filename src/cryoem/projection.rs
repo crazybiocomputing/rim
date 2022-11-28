4a0ff0f875e20d9a5d9cbb5a882b9e9c8806fb91
Louis Montagne <102073161+louis-montagne@users.noreply.github.com>
2022-11-28 10:56:37 +0100

    Update projection.rs

--- src/cryoem/projection.rs
+++ src/cryoem/projection.rs
@@ -16,5 +16,37 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with RIM.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
+//
+// Authors: Luna Meyer, Louis Montagne
+
+
+use crate::image_stack::Image_Stack;
+use crate::grayscale::Gray;
+use crate::vecmath::*;
+use std::f64::consts::PI;
+use nalgebra;
+
+pub fn back_projection3D(phi: f32, theta: f32, psi: f32, n_projections: &Image_Stack<T,C>) -> VolumeProcessor<T,C> {
+    //Create new cube filled with O as pixel values
+    let data = vec![vec![vec![0 as f64;n_projections.get_height()];n_projections.get_width()]; n_projections.n_slices()];
+    let dim: usize = n_projections.get_width();
+    let mut vol = VolumeProcessor(dim, dim, dim, data, Gray::<f32>::new());
+
+    //Rotation matrix
+    let rot_mats: Vec<Matrix3> = vec![rotation_Z(phi), rotation_Y(theta),rotation_Z(psi)];
+    let rotation: Matrix3<f64> = rotation(rot_mats);
+    let invert: Matrix3<f64> = rotation.try_inverse().unwrap();
+
+    for n in 0..n_projections.n_slices(){
+        for z in 0..dim {
+            for y in 0..dim {
+                for x in 0..dim{
+                    let mut position: Vec<f64> = vec![x,y,z]*invert;
+                    //Interpolation
+                    vol.data()[z][y][x]+=  bilinear_interpol(position[0], position[1], n_projections);
+                }
+            }
+        }
+    }
+    return vol;
+}
