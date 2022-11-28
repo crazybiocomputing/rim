6bbe2bfa3d02884ec6a00d39687b94f45db9be4b
Léo Gillet <leo@leogillet.com>
2022-11-28 10:17:56 +0100

    loaded file

--- src/spr.rs
+++ src/spr.rs
@@ -24,10 +24,13 @@
 #![warn(unused)]
 #![warn(dead_code)]
 
+use std::f64::consts::PI;
 use std::process::exit;
+use nalgebra::{Matrix, Matrix3, Rotation3};
 use rim::grayscale::Gray16;
 use rim::image_processor::ImageProcessor;
 use rim::image_stack::ImageStack;
+use rim::io::file_info::FileInfo;
 use rim::io::image_reader::{FileOpener, OutputProcessor};
 use rim::io::text_reader::TextReader;
 use rim::results_table::ResultsTable;
@@ -42,7 +45,7 @@ use rim::results_table::ResultsTable;
 //
 // fn image_stack_projection(ImageStacks ist_stack) -> Vec<ImageStacks> {
 //     let all_img_projection: Vec<ImageStacks> = Vec::new();
-//     for img_stack in ist_stack{
+//     for img_stack in ist_stack {
 //         all_img_projection.push(image_projection(img_stack);
 //     }
 //     return all_img_projection;
@@ -55,30 +58,64 @@ impl SPR {
         return TextReader::open_csv("samples/psi-theta-phi-50.csv", Option::Some(',')).unwrap();
     }
 
-    fn read_image() -> Option<&'static ImageProcessor<u16, Gray16>> {
-        let processor =  FileOpener::open_stack("samples/T1_head_128x128x128.tif", 128, 128, 128);
-        if let OutputProcessor::ShortProcessor(ip) = &processor {
-            return Some(ip)
-        }
-        return None
-    }
-
     pub fn start() -> () {
         let angles: ResultsTable = Self::read_angles();
-        let processor: Option<&ImageProcessor<u16, Gray16>> = Self::read_image();
-        let ip: &Vec<u16>;
-
-        match processor {
-            Some(x) => {
-                ip = x.data();
-            }
-            None => {
-                exit(1);
-            }
+        let proc = FileOpener::open_processor(
+                "./samples/T1_head_128x128x128.tif",
+                128, 128,
+                FileInfo::GRAY16_UNSIGNED
+            );
+        if let OutputProcessor::ShortStack(ip) = proc {
+            println!("{}x{}",ip.get_width(),ip.get_height());
         }
 
-        for val in ip {
-            print!("{}", val);
+        let phi_table: Vec<f64> = angles.get_column_as_floats(String::from("Phi")).unwrap();
+        let psi_table: Vec<f64> = angles.get_column_as_floats(String::from("Psi")).unwrap();
+        let theta_table: Vec<f64> = angles.get_column_as_floats(String::from("Theta")).unwrap();
+
+        for i in 0..phi_table.len() {
+            let phi = phi_table[i]*(PI/180.0); // Rotation around Z
+            let theta = theta_table[i]*(PI/180.0); // Rotation around Y
+            let psi = psi_table[i]*(PI/180.0); // Rotation around Z
+            // Step #1 - compute the resulting rotation matrix (RZ * RY * RZ)
+            /*
+                       Z                            Y                               Z
+            [ cos(psi) sin(psi) 0 ]     [ cos(theta) 0 -sin(theta) ]    [ cos(phi) sin(phi) 0 ]
+            [-sin(psi) cos(psi) 0 ]     [    0       1      0      ]    [-sin(phi) cos(phi) 0 ]
+            [    0        0     1 ]     [ sin(theta) 0      1      ]    [    0        0     1 ]
+             */
+
+            let mat_psi: Matrix3<f64> = Matrix3::new(
+                psi.cos(),      psi.sin(), 0.0,
+                -psi.sin(),     psi.cos(), 0.0,
+                0.0, 0.0, 1.0
+            );
+
+            let mat_psi: Rotation3<f64> = Rotation3::from_matrix(&mat_psi);
+
+            let mat_theta: Matrix3<f64> = Matrix3::new(
+                theta.cos(), 0.0, -theta.sin(),
+                0.0, 1.0, 0.0,
+                theta.sin(), 0.0, 1.0
+            );
+
+            let mat_phi: Matrix3<f64> = Matrix3::new(
+                phi.cos(), phi.sin(), 0.0,
+                -phi.sin(), phi.cos(), 0.0,
+                0.0, 0.0, 1.0
+            );
+
+            let mat = mat_psi * mat_theta * mat_phi;
+            // let rot: Rotation3::from_matrix(mat);
+            let inv = mat.try_inverse().unwrap();
+            // Step #2 -
+            // let proj = stack.get_slice(i);
+            // Step #3 - Backprojection
+            
+            // Step #4 - Apply rotation to backprojection
+            // Step #5 - Add rotated backprojection to output
         }
+        // Step #6 - Return output
+
     }
 }
