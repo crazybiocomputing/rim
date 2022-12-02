d2122f218bb144f8dfd528eeea8e254c2aa21e9e
Charlene Vernant <charlene.vernant@gmail.com>
2022-12-02 22:17:35 +0100

    add rank and find maxima

--- src/bin/cmonlines.rs
+++ src/bin/cmonlines.rs
@@ -181,6 +181,119 @@ fn get_sinogram(filename : &str) -> FloatProcessor{
     op
 
 }
+fn rank(values: &Vec<f32>) ->Vec<f32>{
+    /// https://imagej.nih.gov/ij/developer/source/ij/util/Tools.java.html
+    ///
+    /// Calculates peak positions of 1D array N.Vischer, 06-mar-2017
+    ///
+    /// # Parameters
+    ///
+    /// `xx` : A Vec<f32> containing peaks.
+    /// `tolerance` : Depth of a qualified valley must exceed tolerance.
+    ///                Tolerance must be >= 0. Flat tops are marked at their centers.
+    /// `edgemode` : 0=include, 1=exclude
+    ///              edgeMode = 0 (include edges) peak may be separated by one qualified valley and by a border.
+    ///              edgeMode = 1 (exclude edges) peak must be separated by two qualified valleys
+    ///
+    /// # Returns
+    ///
+    /// Positions of peaks, sorted with decreasing amplitude, as Vect<f32>
+    let len= values.len();
+    let mut indexes= Vec::<f32>::new();
+    let mut data = values.clone();
+    data.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    for value in data{
+        for i in 0..len{
+            if value == values[i]{
+                indexes.push(i as f32);
+            }
+        }
+    }
+    indexes
+}
+fn find_maxima(xx: &Vec<f32>, mut tolerance: f32, edgemode: f32) ->Vec<f32>{
+    /// https://imagej.nih.gov/ij/developer/source/ij/plugin/filter/MaximumFinder.java.html
+    /// Ranks a Vec<f32> from the index of the smallest value to index of tha biggest value
+    ///
+    /// # Parameters
+    ///
+    /// `values` : A Vec<f32> that will be ranked
+    ///
+    /// # Returns
+    ///
+    ///
+    let INCLUDE_EDGE: f32 = 0 as f32;
+    let len: usize = xx.len() as usize;
+    if len < 2 {
+        return Vec::<f32>::with_capacity(0);
+    }
+    if tolerance<0 as f32{
+        tolerance = 0 as f32;
+    }
+    let mut max_positions:Vec<f32> = vec![0.0; len];
+    let mut max: f32 = xx[0];
+    let mut min: f32 = xx[0];
+    let mut max_pos: f32 = 0 as f32;
+    let mut last_max_pos: f32 = -1 as f32;
+    let mut left_valley_found: bool = edgemode as f32 == INCLUDE_EDGE as f32;
+    let mut max_count: f32 = 0 as f32;
+    for i in 0..len{
+        let val: f32 = xx[i];
+        if val > min + tolerance{
+            left_valley_found = true;
+        }
+        if (val > max) & (left_valley_found){
+            max = val;
+            max_pos = i as f32;
+        }
+        if left_valley_found{
+            last_max_pos = max_pos;
+        }
+        if (val < max - tolerance) & (left_valley_found){
+            max_positions[max_count as usize]= max_pos;
+            max_count+=1 as f32;
+            left_valley_found = false;
+            min = val;
+            max = val;
+        }
+        if val < min {
+            min = val;
+            if !left_valley_found{
+                max = val;
+            }
+        }
+
+    }
+    if edgemode == INCLUDE_EDGE{
+        if (max_count> 0 as f32) & (max_positions[max_count as usize-1] != last_max_pos){
+            max_positions[max_count as usize+1] = last_max_pos;
+        }
+        if (max_count == 0 as f32) & (max -min >= tolerance){
+            max_positions[max_count as usize+1] = last_max_pos;
+        }
+    }
+    let mut cropped:Vec<f32> = vec![0.0; max_count as usize];
+    cropped.copy_from_slice(&max_positions[0..max_count as usize]);
+    max_positions = cropped;
+    let mut max_values:Vec<f32> = vec![0.0; max_count as usize];
+    for i in 0..max_count as usize{
+        let mut pos : f32 = max_positions[i];
+        let mut mid_pos : f32 = pos;
+        while (pos < (len - 1) as f32) & (xx[pos as usize] == xx[pos as usize +1]) {
+            mid_pos += 0.5;
+            pos+=1 as f32;
+        }
+        max_positions[i] = mid_pos;
+        max_values[i] = xx[max_positions[i] as usize];
+    }
+    let rank_positions = rank(&max_values);
+    let mut returnArr:Vec<f32> = vec![0.0; max_count as usize];
+    for i in 0..(max_count as usize){
+        let pos: f32 = max_positions[rank_positions[i] as usize];
+        returnArr[max_count as usize- i -1] = pos; //use descending order
+    }
+    returnArr
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -190,9 +303,23 @@ fn main() {
     let sin2 = get_sinogram("./samples/50.raw");
 
     let corr_test = corr_sinogram(&sin1, &sin2);
-    for i in 0..128 {
-        println!("index {}, valeur {}",i, corr_test[i])
+    /*for i in 0..128 {
+        println!("index {}, valeur {}", i, corr_test[i])
+    }*/
+
+    let vectest:Vec<f32> = vec![1.0, 5.0, 10.0, 2.0, 15.0];
+    let vecrank = rank(&vectest);
+    for i in 0..vecrank.len(){
+        println!("rank: {}, valeur vec: {}", vecrank[i], vectest[vecrank[i] as usize])
     }
 
+    // Ici j'ai créé un vecteur avec 3 pics entourés de petites valeurs
+    // Si tu enleves une petite valeur entre 2 grandes ça comptera plus comme 2 pics mais 1 seul
+    // Enfin edgemode 1 quoi
+    let xxtest:Vec<f32> = vec![1.0,500.09,2.0,50.5,3.0,8.0,100.0,0.05];
+    let a :Vec<f32> = find_maxima(&xxtest, 0 as f32, 1 as f32);
+    for i in 0..a.len(){
+        println!("maximum index: {}, valeur vec: {}", a[i], xxtest[a[i] as usize])
+    }
 
 }
