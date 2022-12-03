20ca7b9a818b4943ace5cbfc4acbee588a6f899e
chrislaincoubard <101561839+chrislaincoubard@users.noreply.github.com>
2022-12-03 13:10:34 +0100

    fix cross_correlation

--- src/bin/cmonlines.rs
+++ src/bin/cmonlines.rs
@@ -53,32 +53,32 @@ fn olny_sd_norm(vector : &Vec<f32>) -> Vec<f32> {
     norm_vec
 }
 
+fn get_from_last(vect : &Vec<f32>, index : usize) -> f32 {
+    vect[vect.len() - index]
+}
+
 fn cross_correlation(x : &Vec<f32>, y : &Vec<f32>) -> Vec<f32> {
-    let n = x.len();
-    let mean_x = mean(x);
-    let mean_y = mean(y);
-    let mut sum_x = 0 as f32;
-    let mut sum_y = 0 as f32;
+    let n = x.len()-1;
+    let mut sum = 0 as f32;
     let mut result = Vec::<f32>::new();
-    for i in 0..x.len() {
-        sum_x += f32::pow(x[i] - mean_x, 2);
-        sum_y += f32::pow(y[i] - mean_y, 2);
-    }
-    let denom = (sum_x * sum_y).sqrt();
-    for delay in 0..n {
-        let mut num = 0 as f32;
-        for i in 0..n {
-            let shift_neg = (i + delay);
-            let shift_pos = (i as i32 - delay as i32);
-            println!("{}",shift_pos);
-            if shift_neg < n{
-                num += (x[i] - mean_x) * (y[shift_neg] - mean_y);
-            }
-            if shift_pos > 0 as i32 {
-                num += (x[i] - mean_x) * (y[shift_pos as usize] - mean_y);
-            }
+    let mut temp = Vec::<f32>::new();
+    for delay in 0..=n{
+        sum = 0.0;
+        for i in 0..=delay{
+            sum += x[i] * y[n - delay + i];
         }
-        result.push(num / denom);
+        result.push(sum);
+    }
+    for delay in 0..n{
+        sum = 0.0;
+        for i in 0..=delay {
+            sum += x[n - delay + i] * y[i]
+        }
+        temp.push(sum);
+    }
+    temp.reverse();
+    for i in 0..temp.len() {
+        result.push(temp[i])
     }
     result
 }
@@ -96,12 +96,31 @@ fn corr_1_line(colonne : &Vec<f32>, ip: &FloatProcessor) -> Vec<f32> {
     /// A Vec<f32> containing the cross-correlation between the vector and each column of the FloatProcessor.
     ///
     let mut corr = Vec::<f32>::new();
-        for x in 0..ip.get_width() {
-            let curr_col = get_col(ip, x);
+        for y in 0..ip.get_height() {
+            let curr_col = get_line(ip, y);
             corr.push(euc_dist(colonne, &curr_col));
         }
 
     corr
+}
+
+fn get_line_test(vect : Vec<f32>, index:u32, width : u32) -> Vec<f32> {
+    let mut row = Vec::<f32>::new();
+    for x in 0..width {
+        let index = (x + width * index) as usize;
+        let pixel = vect[index];
+        row.push(pixel);
+    }
+    row
+}
+
+fn get_line(ip: &FloatProcessor, index: u32) -> Vec<f32> {
+    let mut row = Vec::<f32>::new();
+    for x in 0..ip.get_width() {
+        let pixel = (ip.get_pixel_at(x, index).unwrap() as f32);
+        row.push(pixel);
+    }
+    row
 }
 
 fn get_col(ip: &FloatProcessor, index: u32) -> Vec<f32> {
@@ -117,7 +136,7 @@ fn get_col(ip: &FloatProcessor, index: u32) -> Vec<f32> {
     ///
     let mut row : Vec<f32> = Vec::<f32>::new();
     for y in 0..ip.get_height() {
-        let pixel = (ip.get_pixel_at(index as u32, y as u32)).unwrap() as f32;
+        let pixel = (ip.get_pixel_at(index, y)).unwrap() as f32;
         row.push(pixel);
     }
     row
@@ -128,8 +147,8 @@ fn euc_dist(vect1: &Vec<f32>, vect2: &Vec<f32>) -> f32 {
     ///
     /// # Parameters
     ///
-    /// `vect1` : A Vec<f32>, in our case it contains the pixel value of a column of a Singogram
-    /// `vect2` : A Vec<f32>, in our case it contains the pixel value of a column of a Singogram
+    /// `vect1` : A Vec<f32>, in our case it contains the pixel value of a column of a Sinogram
+    /// `vect2` : A Vec<f32>, in our case it contains the pixel value of a column of a Sinogram
     ///
     /// # Returns
     ///
@@ -144,22 +163,13 @@ fn euc_dist(vect1: &Vec<f32>, vect2: &Vec<f32>) -> f32 {
 fn corr_sinogram(ip1 : &FloatProcessor, ip2 : &FloatProcessor) -> Vec<f32> {
     let mut scf = Vec::<f32>::new();
         for x in 0..ip1.get_width() {
-            let col = get_col(&ip1, x);
+            let col = get_line(&ip1, x);
             let mut dist = corr_1_line(&col, &ip2);
             scf.append(&mut dist);
         }
     scf
 }
 
-
-fn percent_diff(num1 : f32, num2 : f32) -> f32 {
-    if num1 > num2 {
-        num1 / num2 * (100 as f32)
-    }
-    else {
-        num2 / num1 * (100 as f32)
-    }
-}
 
 
 fn get_sinogram(filename : &str) -> FloatProcessor{
@@ -177,10 +187,13 @@ fn get_sinogram(filename : &str) -> FloatProcessor{
     let step = 1 as f32;
     let fp = ImageProcessor::new(width, height, vect, Gray32::new());
     let op = Sinogram::new_in_range(&fp, start , end, step);
-    println!(vect.len());
+    for i in 0..op.data.len() {
+        println!("{}",op.data[i])
+    }
     op
 
 }
+
 fn rank(values: &Vec<f32>) ->Vec<f32>{
     /// https://imagej.nih.gov/ij/developer/source/ij/util/Tools.java.html
     ///
@@ -213,7 +226,7 @@ fn rank(values: &Vec<f32>) ->Vec<f32>{
 }
 fn find_maxima(xx: &Vec<f32>, mut tolerance: f32, edgemode: f32) ->Vec<f32>{
     /// https://imagej.nih.gov/ij/developer/source/ij/plugin/filter/MaximumFinder.java.html
-    /// Ranks a Vec<f32> from the index of the smallest value to index of tha biggest value
+    /// Ranks a Vec<f32> from the index of the smallest value to index of the biggest value
     ///
     /// # Parameters
     ///
@@ -299,20 +312,32 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(args);
 
-    let sin1 = get_sinogram("./samples/7.raw");
-    let sin2 = get_sinogram("./samples/50.raw");
-
-    let corr_test = corr_sinogram(&sin1, &sin2);
-    /*for i in 0..128 {
-        println!("index {}, valeur {}", i, corr_test[i])
-    }*/
+    // let sin1 = get_sinogram("./samples/7.raw");
+    // let sin2 = get_sinogram("./samples/50.raw");
+    //
+    // let corr_test = corr_sinogram(&sin1, &sin2);
+    // for i in 0..128 {
+    //     println!("index {}, valeur {}",i, corr_test[i])
+    // }
+    //
+    // let vect1 = vec![10 as f32, 15 as f32, 25 as f32, 30 as f32,
+    //                  20 as f32, 50 as f32, 20 as f32, 12 as f32,
+    //                  25 as f32, 15 as f32, 60 as f32, 28 as f32,
+    //                  22 as f32, 18 as f32, 50 as f32, 14 as f32];
+    // let vect = vec![5 as f32, 10 as f32, 15 as f32, 8 as f32,
+    //                 25 as f32, 20 as f32, 8 as f32, 14 as f32,
+    //                 40 as f32, 12 as f32, 8 as f32, 10 as f32,
+    //                 30 as f32, 15 as f32, 10 as f32, 14 as f32];
+    // let check = get_line_test(vect1,2,4);
+    // for i in 0..check.len() {
+    //     println!("{}", check[i])
+    // }
 
     let vectest:Vec<f32> = vec![1.0, 5.0, 10.0, 2.0, 15.0];
     let vecrank = rank(&vectest);
     for i in 0..vecrank.len(){
         println!("rank: {}, valeur vec: {}", vecrank[i], vectest[vecrank[i] as usize])
     }
-
     // Ici j'ai créé un vecteur avec 3 pics entourés de petites valeurs
     // Si tu enleves une petite valeur entre 2 grandes ça comptera plus comme 2 pics mais 1 seul
     // Enfin edgemode 1 quoi
@@ -322,4 +347,10 @@ fn main() {
         println!("maximum index: {}, valeur vec: {}", a[i], xxtest[a[i] as usize])
     }
 
+    let test_corr1 = vec![0 as f32, 1 as f32, 2 as f32, 3 as f32];
+    let test_corr2 = vec![1 as f32, 1 as f32, 2 as f32, 1 as f32];
+    let test_corrrr = cross_correlation(&test_corr1, &test_corr2);
+    for i in 0..test_corrrr.len() {
+        println!("index {} : valeur : {}", i,test_corrrr[i])
+    }
 }
